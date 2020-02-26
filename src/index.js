e17618416529207316a29fe778e090cabe4b0656
cheroliv <cheroliv@laposte.net>
2020-02-26 10:47:43 +0100

    tatonnement 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import {Provider} from "react-redux";
 
 import registerServiceWorker from "./registerServiceWorker";
@@ -7,17 +8,25 @@ import registerServiceWorker from "./registerServiceWorker";
 import {store} from "./store";
 import AppContainer from "./containers/AppContainer";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+    <BrowserRouter>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
+registerServiceWorker();
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// import * as serviceWorker from './serviceWorker';
+//serviceWorker.unregister();
 
 
 
@@ -25,12 +34,3 @@ serviceWorker.unregister();
 
 
 
-//
-// ReactDOM.render(
-//     <BrowserRouter>
-//         <Provider store={store}>
-//             <AppContainer/>
-//         </Provider>
-//     </BrowserRouter>,
-//     document.getElementById("root")
-// );
